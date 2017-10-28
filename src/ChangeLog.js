44ebabc26d79c38ad52473bd985821635e5e0356
Daniel Beilinson <beilinson@gmail.com>
2017-10-29 00:41:43 +0300

    Change log (WIP).

---
+++ src/ChangeLog.js
@@ -0,0 +1,35 @@
+import { EventEmitter } from 'substance'
+import Parser from './Parser'
+
+class ChangeLog extends EventEmitter {
+  constructor(log) {
+    this.log = log
+    this.parser = new Parser()
+  }
+
+  getVersion() {
+    let version = 0
+    for (let i = this.log.length; i > 0; i--) {
+      const line = this.log[i-1]
+      const data = this.parser.parseLine(line)
+      if(data.type === 'version') {
+        version = data.id
+        break
+      }
+    }
+
+    return version
+  }
+
+  getChangesSince(version) {
+
+  }
+
+  appendChanges(changes) {
+
+  }
+
+  incrementVersion() {
+    this.emit('update')
+  }
+}
