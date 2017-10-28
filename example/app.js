a1e79e1ffac73c8a0bcb8b35ed0ea8e135cf833a
Daniel Beilinson <beilinson@gmail.com>
2017-10-28 17:41:05 +0300

    Silence.

--- example/app.js
+++ example/app.js
@@ -9,11 +9,5 @@ window.onload = () => {
   let importer = configurator.createImporter('html')
   let doc = importer.importDocument('<p>Hello world!</p>')
   let editorSession = new EditorSession(doc, { configurator })
-  let onDocumentChange = function(change) {
-    console.log(JSON.stringify(change.toJSON()))
-  }
   ProseEditorPackage.ProseEditor.mount({ editorSession }, window.document.body)
-  editorSession.onUpdate('document', onDocumentChange, this, {
-    resource: 'document'
-  })
 }
