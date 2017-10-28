8730a805efe55d0c462c885505b462f6f9983154
Daniel Beilinson <beilinson@gmail.com>
2017-10-28 20:53:38 +0300

    Improve parser.

--- src/parser.js
+++ src/parser.js
@@ -1,13 +1,15 @@
 export default class Parser {
   parseLine(line) {
-
-  }
-
-  isVersion() {
-
-  }
-
-  isUserChange() {
-
+    const data = line.split(' ')
+    const type = data.length === 2 ? 'version' : 'change'
+    let res = {
+      type: type,
+      id: data[1]
+    }
+    if(type === 'change') {
+      res.user = data[0]
+      res.change = JSON.parse(data[2])
+    }
+    return res
   }
 }
--- test/parser.test.js
+++ test/parser.test.js
@@ -5,13 +5,14 @@ import { user_a_log } from './fixtures/sample_logs.js'
 const test = module('Parser')
 
 test("Item type", (t) => {
-  let Parser = new Parser()
+  let parser = new Parser()
   let vItem = parser.parseLine(user_a_log[0])
   let uItem = parser.parseLine(user_a_log[1])
 
-  t.equal(parser.isVersion(vItem), true, 'If it is line with version number isVersion should return true')
-  t.equal(parser.isUserChange(vItem), false, 'If it is line with version number isUserChange should return false')
-  t.equal(parser.isVersion(uItem), false, 'If it is line with user change isVersion should return false')
-  t.equal(parser.isUserChange(uItem), true, 'If it is line with user change isUserChange should return true')
+  t.equal(vItem.type, 'version', 'If it is line with version number type should be version')
+  t.equal(uItem.type, 'change', 'If it is line with user change type should be change')
+  t.isNotNil(uItem.change, 'User change line most have change')
+  t.isNil(vItem.change, 'Version line most not have change')
+
   t.end()
 })
