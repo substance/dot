8b5ddd1f4b16d508c6538da0382ab70b973832a0
Daniel Beilinson <beilinson@gmail.com>
2017-10-28 17:02:25 +0300

    Improve test data.

--- example/app.js
+++ example/app.js
@@ -9,5 +9,11 @@ window.onload = () => {
   let importer = configurator.createImporter('html')
   let doc = importer.importDocument('<p>Hello world!</p>')
   let editorSession = new EditorSession(doc, { configurator })
+  let onDocumentChange = function(change) {
+    console.log(JSON.stringify(change.toJSON()))
+  }
   ProseEditorPackage.ProseEditor.mount({ editorSession }, window.document.body)
+  editorSession.onUpdate('document', onDocumentChange, this, {
+    resource: 'document'
+  })
 }
--- test/parser.test.js
+++ test/parser.test.js
@@ -1,13 +1,13 @@
 import { module } from 'substance-test'
 import Parser from '../src/parser'
-import sample from './fixtures/sample_user.json'
+import { user_a_log } from './fixtures/sample_logs.js'
 
 const test = module('Parser')
 
 test("Item type", (t) => {
   let Parser = new Parser()
-  let vItem = parser.parseLine(sample[0])
-  let uItem = parser.parseLine(sample[1])
+  let vItem = parser.parseLine(user_a_log[0])
+  let uItem = parser.parseLine(user_a_log[1])
 
   t.equal(parser.isVersion(vItem), true, 'If it is line with version number isVersion should return true')
   t.equal(parser.isUserChange(vItem), false, 'If it is line with version number isUserChange should return false')
